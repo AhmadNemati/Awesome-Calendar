6f39fb8a6945120768bffdd6d8c2c2424592c5b8
AhmadNemati <nematiprog@gmail.com>
2016-11-23 13:41:30 +0330

    add DayAdpater

--- awesomecalendar/src/main/java/com/ahmadnemati/awesomecalendar/fragment/MonthFragment.java
+++ awesomecalendar/src/main/java/com/ahmadnemati/awesomecalendar/fragment/MonthFragment.java
@@ -3,6 +3,7 @@ package com.ahmadnemati.awesomecalendar.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +18,7 @@ import com.ahmadnemati.awesomecalendar.entity.ConfigEntity;
 public class MonthFragment extends Fragment {
     private View view;
     private ConfigEntity config;
+    private RecyclerView monthRecyclerView;
 
     @Nullable
     @Override
@@ -36,11 +38,11 @@ public class MonthFragment extends Fragment {
         Bundle args = new Bundle();
         MonthFragment fragment = new MonthFragment();
         fragment.setArguments(args);
-        fragment.config=config;
+        fragment.config = config;
         return fragment;
     }
 
     private void init() {
-
+        monthRecyclerView = (RecyclerView) view.findViewById(R.id.month_list);
     }
 }
